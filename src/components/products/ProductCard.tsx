01246632546048ef614f23cf488e663297a2e0c9
mattiaTuri <turina.mattia@gmail.com>
2023-11-20 13:49:10 +0100

    added border on selected badge

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -5,16 +5,33 @@ import Tooltip from "components/shared/Tooltip";
 import ToggleSwitch from "components/shared/ToggleSwitch";
 import CheckBox from "components/shared/CheckBox";
 import { allColors, changeColor } from "utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ProductCard({ product }: { product: ProductProps }) {
   const { id, type, amount, action, active, linked, selectedColor } = product;
   const [color, setColor] = useState<Colors>(selectedColor);
 
-  const changeBadgeColor = (color: string) => {
+  useEffect(() => {
+    const col = changeColor[selectedColor];
+    document.querySelector<HTMLElement>(
+      `#badgeColoursGroup-${id} > #${col}`
+    )!.style.border = "1.5px solid #B0B0B0";
+  }, []);
+
+  const changeBadgeColor = (e: any, color: string) => {
     const colorSplitted = color.split("-");
     const col = colorSplitted[1] as Colors;
     setColor(col);
+    const p = document.querySelectorAll<HTMLElement>(
+      `#badgeColoursGroup-${id} > div`
+    );
+    document
+      .querySelectorAll<HTMLElement>(`#badgeColoursGroup-${id} > div`)
+      .forEach((elem: HTMLElement) => {
+        elem.style.border = "0px";
+      });
+
+    e.target.style.border = "1.5px solid #B0B0B0";
   };
 
   return (
@@ -41,12 +58,13 @@ function ProductCard({ product }: { product: ProductProps }) {
         <p className="text-[14px] text-[#3B755F] leading-[17.01px]">
           Badge colour
         </p>
-        <div className="flex gap-1">
+        <div id={`badgeColoursGroup-${id}`} className="flex gap-1">
           {allColors().map((color: string, index: number) => (
             <div
               key={index}
+              id={color}
               className={`w-[16px] h-[16px] ${color} hover:opacity-80`}
-              onClick={() => changeBadgeColor(color)}
+              onClick={(e) => changeBadgeColor(e, color)}
             ></div>
           ))}
         </div>
